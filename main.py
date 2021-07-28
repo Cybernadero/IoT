d4e1281d0d0332c26b9d9377c0aa8078f7e6dfee
Angel-Davila <a6519150005@utch.edu.mx>
2021-07-28 14:21:07 -0600

    api structure finished

--- main.py
+++ main.py
@@ -1,27 +1,66 @@
+  
 from flask import Flask, jsonify, request
-from flask_restful import Api
 from flask_cors import CORS
+from flask_restful import Api
 from sensors import Sensors
+
 import db_config as database
+
 
 app=Flask(__name__)
 api=Api(app)
 CORS(app)
 
+@app.route("/createGreenhouse/", methods=['GET'])
+def createGreenhouse():
 
-@app.route('/option/', methods=["GET"])
-def option():
-    IdSensor = request.args.get("IdSensor")
-    SensorValue = request.args.get("SensorValue")
-    response = str(database.db.sensors.insert_one({
-        'IdSensor':IdSensor,
-        'SensorValue':SensorValue
-        }).inserted_id)
-    return jsonify({"IdSensor": IdSensor, "SensorValue": SensorValue})
+    IdArduino = request.args.get("IdArduino")
+    flag = Sensors.check_if_arduino_exists(IdArduino)
+    print(flag)
+    if(flag):
+        date = request.args.get("date")
+        response = str(database.db.sensors.insert_one(
+                {
+                    'IdArduino':IdArduino,
+                    'temperature':0,
+                    'humidity':0,
+                    'lastStatus':date,
+                    'measurements':[]
+                }
+            ).inserted_id)
+
+    if flag:
+        return jsonify({"response":response, "IdArduino":IdArduino, "date":date})
+    else:
+        return jsonify({'message':'Arduino already exists'})
 
 
+@app.route("/insertData/", methods=['GET'])
+def insert():
+    IdArduino = request.args.get("IdArduino")
+    temperature = request.args.get("temperature")
+    humidity = request.args.get("humidity")
+    date = request.args.get("date")
 
-api.add_resource(Sensors, '/new/','/<string:by>:<string:data>/')    
+    database.db.sensors.update_one({'IdArduino':IdArduino},
+        {
+            '$push':{
+            'measurements':{
+                'temperature':temperature,
+                'humidity':humidity,
+                'date':date
+            }},
+            '$set':{
+            'temperature':temperature,
+            'humidity':humidity,
+            'date':date,
+            'lastUpdate':date,
+            }
+        })
+
+    return jsonify({"IdArduino":IdArduino,"temperature":temperature,"humidity":humidity, "date":date})
+
+api.add_resource(Sensors, '/delete/')    
 
 if __name__ == '__main__':
     app.run(load_dotenv=True)
--- sensors.py
+++ sensors.py
@@ -1,32 +1,16 @@
-from flask import Flask, json, jsonify, request
-from flask_restful import Resource, abort
-from flask_pymongo import pymongo
-from bson.json_util import ObjectId
+from flask_restful import Resource
+
 import db_config as database
 
 
-class Sensors(Resource):
-   
-    def get(self, by, data):
-        response = self.abort_if_not_exist(by, data)
-        response['_id'] = str(response['_id'])
-        return jsonify(response)
+class Sensors(Resource): 
+    def delete(self):
+        return database.db.sensors.delete_many({}).deleted_count
 
-    def post(self):
-        _id = str(database.db.sensors.insert_one({
-            'IdSensor':request.json['IdSensor'],
-            'SensorValue':request.json['SensorValue'],
-        }).inserted_id)
-        return jsonify({"_id":_id})
+    def check_if_arduino_exists(data):
+        response = database.db.sensors.find_one({'IdArduino':data})
 
-
-    def abort_if_not_exist(self,by,data):
-        if by == "_id":
-            response = database.db.sensors.find_one({"_id":ObjectId(data)})
-        else:
-            response = database.db.sensors.find_one({f"{by}": data})
-            
         if response:
-            return response
+            return False
         else:
-            abort(jsonify({"status":404, f"{by}":f"{data} not found"}))
+            return True
